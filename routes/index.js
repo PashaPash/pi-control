437727bedfd825d3691fb79728e3866731b89610
Pavel Pochobut <pavel.pochobut@gmail.com>
2014-05-17 11:05:43 +0300

    added fancy relay buttons

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@
  */
 var gpio = require('pi-gpio');
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
   // gpio.open(1,"out", null);
-  res.render('index', { title: 'PI Home' });
+  var relays = [
+    { name: "lights 1", pin: 22, on: true },
+    { name: "lights 2", pin: 11, on: true },
+    { name: "CO2",      pin: 13, on: false },
+    { name: "unused 4", pin: 15, on: false },
+    { name: "unused 5", pin: 16, on: true },
+    { name: "unused 6", pin: 18, on: false }
+  ];
+  res.render('index', { title: 'PI Home', relays: relays });
 };
