f813d647903b7f12a0044657e25c976f7b674446
Pavel Pochobut <pavel.pochobut@gmail.com>
2014-05-17 12:58:51 +0300

    on/off handling

--- public/javascripts/pi.js
+++ public/javascripts/pi.js
@@ -2,11 +2,16 @@ $(function () {
   $('.relay-switch').click(function () {
     var btn = $(this);
     var pin = btn.data('relay-pin');
-    var state = btn.data('relay-on');
+    var state = btn.data('relay-state');
 
     btn.addClass('active')
-    btn.addClass(state ? 'btn-success' : 'btn-danger');
+    btn.addClass(state == 'on' ? 'btn-success' : 'btn-danger');
     btn.removeClass('btn-default');
+
+    $.post('/relay', {
+      pin: pin,
+      state: state
+    });
 
     var secondBtn = $('.relay-switch[data-relay-pin=' + pin + ']')
       .not(btn)
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,44 @@
  */
 var gpio = require('pi-gpio');
 
+var relays = [
+  { name: "lights 1", pin: 22 },
+  { name: "lights 2", pin: 11 },
+  { name: "CO2",      pin: 13 },
+  { name: "unused 4", pin: 15 },
+  { name: "unused 5", pin: 16 },
+  { name: "unused 6", pin: 18 }
+];
+
+
 exports.index = function (req, res) {
-  // gpio.open(1,"out", null);
-  var relays = [
-    { name: "lights 1", pin: 22, on: true },
-    { name: "lights 2", pin: 11, on: true },
-    { name: "CO2",      pin: 13, on: false },
-    { name: "unused 4", pin: 15, on: false },
-    { name: "unused 5", pin: 16, on: true },
-    { name: "unused 6", pin: 18, on: false }
-  ];
   res.render('index', { title: 'PI Home', relays: relays });
+};
+
+function isValidPin(pin)
+{
+  for (var i in relays) {
+    if (relays[i].pin == pin) {
+      return true;
+    }
+  }
+  return false;
+}
+
+exports.relay = function (req, res) {
+  var pin = parseInt(req.param("pin"));
+  var state = req.param("state") == 'on' ? 1 : 0;
+
+  if (isValidPin(pin)) {
+    gpio.open(pin, "output", function (err) {
+      gpio.write(pin, state, function () {
+        gpio.close(pin);
+      });
+    });
+
+    res.send(pin + ' set to ' + state);
+  }
+  else {
+    throw 'Invalid pin: ' + pin;
+  }
 };
