14952bc5507ce278585fc87e9143ee77c4a332e0
Pavel Pochobut <pavel.pochobut@gmail.com>
2019-02-02 21:45:43 +0300

    config updated

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ var config = {};
 
 config.relay = [
    {
-     name: 'Spot Light',
-     on: [10, 00],
-     off: [20, 00]
+     name: 'R1',
+    //  on: [10, 00],
+    //  off: [10, 05]
    },
    {
      name: 'Air Pump',
-     on: [6, 00],
+     on: [4, 00],
      off: [13, 30]
    },
    {
@@ -17,19 +17,19 @@ config.relay = [
      off: [23, 10]
    },
    {
-     name: 'Front Light',
+     name: 'Led Light',
      on: [13, 30],
      off: [23, 00]
    },
    {
      name: 'CO2',
      on: [10, 00],
-     off: [22, 00]
+     off: [21, 00]
    },
    {
-     name: 'Back Light',
-     on: [13, 30],
-     off: [23, 00]
+     name: 'R6',
+    //  on: [10, 00],
+    //  off: [10, 05]
    },
 ];
 
